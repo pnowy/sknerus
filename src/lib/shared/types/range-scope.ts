6571306dc4f6500b17ef5165d143a555df4922ab
Przemek Nowak <przemek.nowak.pl@gmail.com>
2026-04-30 12:30:10 +0200

    feat: add RangeScope enum for time range categorization

---
+++ src/lib/shared/types/range-scope.ts
@@ -0,0 +1,10 @@
+export enum RangeScope {
+  Month = 'month',
+  Quarter = 'quarter',
+  Year = 'year',
+  Ytd = 'ytd',
+  LastYear = 'lastyear',
+  ThreeYears = '3years',
+  FiveYears = '5years',
+  All = 'all',
+}
