48d27e33a9f9c702a440c28220cfbf5fe0458714
Przemek Nowak <przemek.nowak.pl@gmail.com>
2026-07-18 12:04:36 +0200

    chore: simplify plugin array formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,13 +14,7 @@ const config = defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [
-    devtools(),
-    nitro(),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
+  plugins: [devtools(), nitro(), tailwindcss(), tanstackStart(), viteReact()],
 })
 
 export default config
